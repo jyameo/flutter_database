b328632a6e8ada106f6c6453f703727155be3aa1
Jason Wolf <jessy.yameogo@gmail.com>
2019-06-03 03:04:16 -0400

    finalize crud test

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_crud_database/models/user.dart';
+import 'package:flutter_crud_database/utils/database_helper.dart';
 
-void main() {
+List _users;
+
+void main() async {
+  var db = new DatabaseHelper();
+
+  // Create user
+
+//  await db.saveUser(User("Gerard", "loyc"));
+  // Get all users
+  _users = await db.getAllUsers();
+
+  for (int i = 0; i < _users.length; i++) {
+    User user = User.map(_users[i]);
+
+    print("Username: ${user.username}");
+  }
+
   runApp(MaterialApp(
     title: "DATABASE",
     home: new Home(),
@@ -15,6 +33,22 @@ class Home extends StatelessWidget {
         title: Text('Database'),
         centerTitle: true,
       ),
+      body: ListView.builder(
+          itemCount: _users.length,
+          itemBuilder: (_, int position) {
+            return Card(
+                color: Colors.white,
+                elevation: 2.0,
+                child: ListTile(
+                  leading: CircleAvatar(
+                      child: Text(
+                          "${User.map(_users[position]).username.substring(0, 1)}")),
+                  title: Text("User: ${User.map(_users[position]).username}"),
+                  subtitle: Text("Id: ${User.map(_users[position]).id}"),
+                  onTap: () =>
+                      debugPrint("${User.map(_users[position]).password}"),
+                ));
+          }),
     );
   }
 }
