f7a2d49bb475df7064f8a340aa8c3511a885c252
Jason Wolf <jessy.yameogo@gmail.com>
2019-06-03 01:48:41 -0400

    DONE: user crud ops

--- lib/utils/database_helper.dart
+++ lib/utils/database_helper.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:flutter_crud_database/models/user.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
@@ -9,7 +10,7 @@ class DatabaseHelper {
   static final DatabaseHelper _instance = new DatabaseHelper.internal();
 
   factory DatabaseHelper() => _instance;
-  final String tableUser = "userTable";
+  final String tableName = "userTable";
   final String columnId = "id";
   final String columnUsername = "username";
   final String columnPassword = "password";
@@ -31,13 +32,61 @@ class DatabaseHelper {
     String path = join(documentDirectory.path, "maindb.db");
 
     var my_db = await openDatabase(path, version: 1, onCreate: _onCreate);
+    return my_db;
   }
 
   void _onCreate(Database db, int version) async {
     await db.execute(""
-        "Create Table $tableUser("
+        "Create Table $tableName("
         "$columnId INTEGER PRIMARY KEY, "
         "$columnUsername TEXT, "
         "$columnPassword TEXT)");
+  }
+
+  //Create
+  Future<int> saveUser(User user) async {
+    var dbClient = await db;
+    int res = await dbClient.insert('$tableName', user.toMap());
+    return res;
+  }
+
+  Future<List> getAllUsers() async {
+    var dbClient = await db;
+    var res = await dbClient.rawQuery("SELECT * FROM $tableName");
+    return res;
+  }
+
+  Future<int> getCount() async {
+    var dbClient = await db;
+    return Sqflite.firstIntValue(await dbClient.rawQuery(""
+        "SELECT COUNT(*) FROM $tableName"));
+  }
+
+  Future<User> getUser(int id) async {
+    var dbClient = await db;
+
+    var res = await dbClient.rawQuery(""
+        "SELECT * FROM $tableName "
+        "WHERE $columnId = $id");
+    if (res.length == 0) return null;
+
+    return new User.fromMap(res.first);
+  }
+
+  Future<int> deleteUser(int id) async {
+    var dbClient = await db;
+    return await dbClient
+        .delete(tableName, where: "$columnId = ?", whereArgs: [id]);
+  }
+
+  Future<int> updateUser(User user) async {
+    var dbClient = await db;
+    return dbClient.update(tableName, user.toMap(),
+        where: "$columnId = ?", whereArgs: [user.id]);
+  }
+
+  Future close() async {
+    var dbClient = await db;
+    return dbClient.close();
   }
 }
